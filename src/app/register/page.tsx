5c7f977d1148f8acafd9dff614d069483c3c8df5
faizan1405 <faizankhan1405@gmail.com>
2026-08-06 02:03:13 +0530

    j

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,5 @@
-import React from 'react';
-import { redirect } from 'next/navigation';
-import { auth } from '@/auth';
 import RegisterPageClient from './RegisterPageClient';
 
-export default async function RegisterPage() {
-  const session = await auth();
-
-  if (!session?.user?.id) {
-    redirect('/login?callbackUrl=/register');
-  }
-
+export default function RegisterPage() {
   return <RegisterPageClient />;
 }
